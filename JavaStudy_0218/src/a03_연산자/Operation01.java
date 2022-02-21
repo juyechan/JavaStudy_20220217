e11b63563ab6f92ee731f761c296632c1766e57d
sapa4000 <sapa4000@gmail.com>
2022-02-21 23:50:26 +0900

    연사자 수업내용

---
+++ JavaStudy_0218/src/a03_연산자/Operation01.java
@@ -0,0 +1,18 @@
+package a03_연산자;
+
+public class Operation01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int score = 10;
+		//score = score + 1;
+		//score++; // 증감연산자 
+		
+		System.out.println(score--);
+		System.out.println(score++);
+		System.out.println(score);
+		
+		
+	}
+
+}
---
+++ JavaStudy_0218/src/a03_연산자/Operation02.java
@@ -0,0 +1,25 @@
+package a03_연산자;
+
+public class Operation02 {
+
+	public static void main(String[] args) {
+		int dan =2;
+		int num =1;
+		
+		int result = dan*num;
+		
+		System.out.println(dan + "X" +num + "=" +result);
+		
+		int year = 2022;
+		int month = 2;
+		int day = 21;
+		
+		System.out.println(++year + "년" + ++month +"월" + ++day +"일");
+		
+		
+		
+
+	}
+
+}
+	
